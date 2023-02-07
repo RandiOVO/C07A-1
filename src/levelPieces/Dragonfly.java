04e43fa9ac61428c8e986f5d3d88b322218e93e0
Spencer Hamilton <spencernhamilton@gmail.com>
2023-02-06 21:36:57 -0700

    Dragonfly class implemented

--- src/levelPieces/Dragonfly.java
+++ src/levelPieces/Dragonfly.java
@@ -1,10 +1,15 @@
 package levelPieces;
 
 import gameEngine.Drawable;
+import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
 import gameEngine.Moveable;
 
+import java.util.Random;
+
 public class Dragonfly extends GamePiece implements Moveable {
+    private boolean moveRight;
+
     /**
      * Constructor for the game piece
      *
@@ -17,5 +22,21 @@ public class Dragonfly extends GamePiece implements Moveable {
     @Override
     public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
         return InteractionResult.NONE;
+    }
+
+    @Override
+    public void move(Drawable[] gameBoard, int playerLocation) {
+        Random rand = new Random();
+        int currentPos = this.getLocation();
+        int distance = rand.nextInt(4);
+        int newPos;
+        boolean direction = rand.nextBoolean();
+        if (direction) {
+            newPos = (currentPos - distance) < 0 ? 0 : currentPos - distance;
+        } else {
+            newPos = (currentPos + distance) < GameEngine.BOARD_SIZE ? currentPos + distance : GameEngine.BOARD_SIZE - 1;
+        }
+        this.setLocation(newPos);
+        gameBoard[newPos] = this;
     }
 }
