01727c9e2386d26e9418fa24c8da18fdcd7c8dbf
Spencer Hamilton <spencernhamilton@gmail.com>
2023-02-10 17:49:39 -0700

    made reward test and updated ignore

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -1,14 +1,25 @@
 package tests;
 
-import static org.junit.Assert.*;
-
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import gameEngine.Player;
+import levelPieces.Reward;
 import org.junit.Test;
+import org.junit.Assert.*;
+
+import static junit.framework.Assert.assertEquals;
 
 public class TestInteractions {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void rewardInteraction() {
+        Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+        Reward reward = new Reward(0);
+        Player player = new Player(0);
+        InteractionResult result = reward.interact(gameBoard, player.getLocation());
+        assertEquals(result, InteractionResult.GET_POINT);
+    }
+
 
 }
