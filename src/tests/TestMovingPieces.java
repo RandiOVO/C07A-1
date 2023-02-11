343d2de86a95c0cba8c0aa70e2509a8eedf0a9f3
Spencer Hamilton <spencernhamilton@gmail.com>
2023-02-10 18:34:42 -0700

    soilder moving tests finished

--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -2,13 +2,32 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import gameEngine.Player;
+import levelPieces.Solider;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
 import org.junit.Test;
 
 public class TestMovingPieces {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testSoliderMoving() {
+        Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+        Solider solider = new Solider(0);
+        Player player = new Player(GameEngine.BOARD_SIZE - 1);
+        solider.move(gameBoard, player.getLocation());
+        assertEquals(solider.getLocation(), 0);
+        solider.move(gameBoard, player.getLocation()); // Ensure the solider changes direction
+        assertEquals(solider.getLocation(), 1);
+
+        while (solider.getLocation() != GameEngine.BOARD_SIZE - 1)
+            solider.move(gameBoard, player.getLocation());
+        assertEquals(solider.getLocation(), GameEngine.BOARD_SIZE - 1);
+        solider.move(gameBoard, player.getLocation());
+        solider.move(gameBoard, player.getLocation()); // Ensure they turn around on the other side
+        assertEquals(solider.getLocation(), GameEngine.BOARD_SIZE - 2);
+
+    }
 
 }
